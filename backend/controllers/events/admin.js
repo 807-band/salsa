518b4fe7d6ada37e282e3902752b4640966557a6
bcollard <bryce.collard@gmail.com>
2021-05-10 13:25:54 -0700

    removing print statements

--- backend/controllers/events/admin.js
+++ backend/controllers/events/admin.js
@@ -1,12 +1,9 @@
 const db = require('../../config/db');
 
 module.exports.create = async (req, res) => {
-  console.log(req.body.title);
-  console.log(req.body.startTime);
   db.execute('INSERT INTO Events (title, startTime) VALUES (?, ?)',
     [req.body.title, req.body.startTime],
     (err, results) => {
-      console.log(results);
       req.body.eventID = results.insertId;
       res.jsonp(req.body);
     });
