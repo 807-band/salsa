e2cde110f6179833c3b608ea23e3d85c7082701e
bcollard <bryce.collard@gmail.com>
2022-04-05 10:21:05 -0700

    updated MySQL setup

--- backend/dbscripts/init.sql
+++ backend/dbscripts/init.sql
@@ -1,2 +1,8 @@
-CREATE DATABASE 807web;
-USE 807web;
+CREATE DATABASE temp;
+USE temp;
+
+source ~/807/salsa/backend/dbscripts/create_station_tables.sql
+source ~/807/salsa/backend/dbscripts/create_station_triggers.sql
+source ~/807/salsa/backend/dbscripts/create_users_tables.sql
+source ~/807/salsa/backend/dbscripts/create_evaluation_tables.sql
+source ~/807/salsa/backend/dbscripts/create_events_tables.sql
