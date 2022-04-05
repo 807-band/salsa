8d737f812cc1a94d8d1491f7857fd7842e170a86
bcollard <bryce.collard@gmail.com>
2022-04-05 10:27:41 -0700

    fixed init.sql

--- backend/dbscripts/init.sql
+++ backend/dbscripts/init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE temp;
-USE temp;
+CREATE DATABASE 807web;
+USE 807web;
 
-source ~/807/salsa/backend/dbscripts/create_station_tables.sql
-source ~/807/salsa/backend/dbscripts/create_station_triggers.sql
-source ~/807/salsa/backend/dbscripts/create_users_tables.sql
-source ~/807/salsa/backend/dbscripts/create_evaluation_tables.sql
-source ~/807/salsa/backend/dbscripts/create_events_tables.sql
+source dbscripts/create_station_tables.sql
+source dbscripts/create_station_triggers.sql
+source dbscripts/create_users_tables.sql
+source dbscripts/create_evaluation_tables.sql
+source dbscripts/create_events_tables.sql
